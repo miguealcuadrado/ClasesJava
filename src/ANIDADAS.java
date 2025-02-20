ac54d5c0291b25676a831e2470c0f9492cb84d22
Miguel <marodasm@correo.iue.edu.co>
2025-02-20 17:08:01 -0500

    ANIDADAS COMPLETAS

--- src/ANIDADAS.java
+++ src/ANIDADAS.java
@@ -1,3 +1,37 @@
 public class ANIDADAS {
+    public static void main(String[] args) {
+
+        int operacion = 3;
+        int Num_1 = 15;
+        int Num_2 = 13;
+        int Resultado = 0;
+
+        if (operacion >= 5) {
+            System.out.println("Recuerde por favor ingresar un valor entre 1 y 4");
+
+        }else if(operacion == 1) {
+            Resultado = Num_1 + Num_2;
+            System.out.println("El Resultado es " + Resultado);
+        
+        }else if(operacion == 2) {
+            Resultado = Num_1 - Num_2;
+            System.out.println("El Resultado es " + Resultado);
+            
+        }else if(operacion == 3) {
+            Resultado = Num_1 * Num_2;
+            System.out.println("El Resultado es " + Resultado);
+
+        }else if(operacion == 4) {
+            Resultado = Num_1 / Num_2;
+            System.out.println("El Resultado es " + Resultado);
+                
+        }else {
+            System.out.println("Recuerde ingresar un valor valido");
+        }
+
+
+
+    
+    }
 
 }
